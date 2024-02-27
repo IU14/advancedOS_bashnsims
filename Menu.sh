5fb813fdb5535d8781f2dec6017e5fd18686b22b
IU14 <75305549+IU14@users.noreply.github.com>
2024-02-27 10:30:16 +0000

    Uploading updated menu.sh script

--- Menu.sh
+++ Menu.sh
@@ -1,36 +1,64 @@
 #!/bin/sh
 
-#This is skel code
+
+#defining colours for the menu using asni codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+RESET='\033[0m'
+
+#clearing screen
+clear
+
+#setting to full screen
+
 
 #Menu Display & Select
+
 Menu()
 {
-echo "Make your selection or type bye to exit:" 
-echo "1 for FIFO"
-echo "2 for LIFO"
-echo "BYE for exit"
-echo "Please Enter Selection:"
-read Sel
-MenuSel $Sel
+	echo -e "Make your selection or type bye to exit:" 
+	echo -e "${BLUE}1 for FIFO${RESET}"
+	echo -e "${GREEN}2 for LIFO${RESET}"
+	echo -e "${RED}BYE for Exit${RESET}"
+	echo -e "Please Enter Selection:"
+	read Sel
+	MenuSel "$Sel"
 }
-
 
 
 #Menu case
 MenuSel()
 {
 
-case $1 in
+case $(echo "$1" | tr '[:upper:]' '[:lower:]') in
 	1) sh FIFO.sh;;
         2) sh LIFO.sh;;
-	BYE) exit;;
-	*) echo "Invalid Selection"
+	bye) ExitFunc;;
+	*) echo -e "${MAGENTA}Invalid Selection${RESET}"
 	sleep 1
 	Menu;;
 esac
 }
 
-
+ExitFunc()
+{
+while true; do
+    echo "Do you really want to exit? (Y/n)"
+    read choice
+    case "$(echo "$choice" | tr '[:upper:]' '[:lower:]')" in
+        y|yes) 
+            echo "GoodBye"
+	    exit ;;
+        n|no) 
+            echo "Continuing"
+            break ;;
+        *) 
+            echo "Invalid choice. Please enter Y or n." ;;
+    esac
+done
+}
 #####################
 ### RUNNING CODE ####
 #####################
@@ -39,6 +67,9 @@ esac
 echo "Please Enter Username"
 read Uname
 
-while true;do
-	Menu
-done
+#runs within a clean subshell
+(
+	while true;do
+		Menu
+	done
+)
